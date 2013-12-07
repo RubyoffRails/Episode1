4f8db120c48eb61e5ca6d73813d988f7e4c0b4a6
noahpatterson <noah@noah-patterson.com>
2013-12-07 18:12:02 -0500

    add human class

---
+++ zoo.rb
@@ -0,0 +1,87 @@
+#Zoo
+
+module Animal
+
+	def eat(food)
+		@meals ||= 0
+		if likes?(food)
+			@meals += 1
+			true
+		else
+			false
+		end
+	end
+
+	def likes?(food)
+		acceptable_food.include?(food)
+	end
+
+	def acceptable_food
+		[]
+	end
+
+	def full?
+		false
+	end
+
+end
+
+
+class Panda
+	include Animal
+
+	def acceptable_food
+		[Bamboo.new]
+	end
+
+	def full?
+		@meals > 30
+	end
+
+end
+
+class Lion
+	include Animal
+
+	def acceptable_food
+		[Wildebeests.new, Zeebras.new]
+	end
+
+	def full?
+		@meals > 10
+	end
+end
+
+
+class Food
+
+	def ==(other)
+		other.is_a? self.class
+	end
+
+end
+
+class Tacos < Food; end
+class Bacon < Food; end
+class Wildebeests < Food; end
+class Zeebras < Food; end
+class Bamboo < Food; end
+
+class Zookeeper
+	def feed(args={})
+		food = args.fetch(:food)
+		panda = args.fetch(:to)
+		panda.eat(food)
+	end
+
+end
+
+class Human
+	include Animal
+
+	def acceptable_food
+		[Bacon.new, Tacos.new]
+	end
+
+end
+
---
+++ zoo_spec.rb
@@ -0,0 +1,99 @@
+# Zoo spec file
+
+require "./zoo"
+require "rspec"
+require '../../spec_helper'
+
+class Grasshoppers < Food; end
+class Salad < Food; end
+
+describe Tacos do
+	it "should know all tacos are equal" do
+		(Tacos.new == Tacos.new).should be_true
+	end
+end
+
+describe Panda do
+
+	it "should like bamboo" do
+		Panda.new.likes?(Bamboo.new).should eq(true)
+	end
+
+	it "should not like grasshoppers" do
+		Panda.new.likes?(Grasshoppers.new).should eq(false)
+	end
+
+	it "should be able to eat the food" do
+		Panda.new.eat(Bamboo.new).should be_true
+	end
+
+	it "should be full after eating 30 bamboo" do
+		panda = Panda.new
+		31.times do
+			panda.eat(Bamboo.new)	
+		end
+		panda.should be_full
+	end
+
+	it "should not be full after 1" do
+		panda = Panda.new
+		panda.eat(Bamboo.new)	
+		panda.should_not be_full
+	end
+end
+
+describe Lion do
+	it "should like wildebeests" do
+		Lion.new.likes?(Wildebeests.new).should eq(true)
+	end
+
+	it "should like zeebras" do
+		Lion.new.likes?(Zeebras.new).should eq(true)
+	end
+
+	it "should not like salad" do
+		Lion.new.likes?(Salad.new).should eq(false)
+	end
+
+	it "should take 11 meals to be full" do
+		lion = Lion.new
+		lion.eat(Zeebras.new)
+		lion.should_not be_full
+	end
+	it "should take 11 meals to be full" do
+		lion = Lion.new
+		11.times do
+			lion.eat(Zeebras.new)
+		end
+		lion.should be_full
+	end
+end
+
+describe Zookeeper do
+	it "should be able to feed bamboo to the pandas" do
+		panda = Panda.new
+		panda.should_receive(:eat).with(:bamboo)
+		Zookeeper.new.feed(food: :bamboo, to: panda)
+	end
+
+	it "should be able to feed zeebras to the lions" do
+		lion = Lion.new
+		lion.should_receive(:eat).with(:zeebras)
+		Zookeeper.new.feed(food: :zeebras, to: lion)
+	end
+end
+
+describe Human do
+	it "should like bacon" do
+		expect(Human.new.likes?(Bacon.new)).to eq(true)
+	end
+
+	it "should like tacos" do
+		expect(Human.new.likes?(Tacos.new)).to eq(true)
+	end
+
+	it "should not like bamboo" do
+		expect(Human.new.likes?(Bamboo.new)).to eq(false)
+	end
+end
+
